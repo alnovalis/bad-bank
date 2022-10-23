96d720e6a7e0483fdef6905b19f553a26c2f5360
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-23 10:28:45 -0400

    Update home.js

--- home.js
+++ home.js
@@ -4,8 +4,8 @@ function Home() {
       bgcolor="primary"
       txtcolor="light"
       header="BadBank Landing Module"
-      title="Welcome to Bad Bank"
-      text="Interact with the menu to begin!"
+      title="Welcome to Bad Bank!"
+      text="Interact with the menu to begin."
       body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
     />
   );
