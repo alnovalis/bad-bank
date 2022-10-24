59cca0ebcd054d42f187e77ab28b81172b899f3e
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-24 11:40:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function Spa() {
           ],
         }}
       >
-        <div className="container" style={{ padding: "100px" }}>
+        <div className="container" style={{ padding: "20px" }}>
           <Route path="/" exact component={Home} />
           <Route path="/CreateAccount/" component={CreateAccount} />
           <Route path="/login/" component={Login} />
