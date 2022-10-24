f21e53cf3083994e2e68be44f122ab4d4343ab3b
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-24 08:59:32 -0400

    Update createaccount.js

--- createaccount.js
+++ createaccount.js
@@ -8,7 +8,7 @@ function CreateAccount(){
 
   function validate(field, label){
       if (!field) {
-        setStatus('Error: ' + label);
+        setStatus('Error: ' + 'missing' + label);
         setTimeout(() => setStatus(''),3000);
         return false;
       }
