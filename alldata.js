424658c1c00b5e39d4f3fdf1b2271aea6cba0da9
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-24 08:08:14 -0400

    Update alldata.js

--- alldata.js
+++ alldata.js
@@ -1,18 +1,13 @@
-function AllData() {
+  function AllData(){
   const ctx = React.useContext(UserContext);
-
-  let users = [...ctx.users];
-  console.log(JSON.stringify(users));
-
-  function userNumber(i) {
-    return `User Account ${i + 1} Table`;
-  }
-
-  function userInfo(user) {
-    return [user.name, user.email, user.password, user.balance];
-  }
-
   return (
+    <>
+    <h5>All Data in Store</h5>
+    {JSON.stringify(ctx)}<br/>
+    </>
+  );
+}
+return (
     <>
       {users.map((user, i) => (
         <Card
