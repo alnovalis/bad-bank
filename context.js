616d131cf2d025bd7891d02bb169dbe020d41956
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-24 11:36:15 -0400

    Update context.js

--- context.js
+++ context.js
@@ -154,7 +154,7 @@ function Card(props) {
     return (
         <div className={classes()} style={{maxWidth: "18rem"}}>
             <div className="card-header text-center">{props.header}</div>
-            <div className="card-body">
+            <div className="card-body text-center">
                 {props.title && (<h5 className="card-title">{props.title}</h5>)}
                 {props.text && (<p className="card-text">{props.text}</p>)}
                 {props.body}
