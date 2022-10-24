c697dd0e3ff490aa8bd01052f58eb69e16e3d30f
Ashlea Novalis <104088564+alnovalis@users.noreply.github.com>
2022-10-24 10:34:21 -0400

    Update home.js

--- home.js
+++ home.js
@@ -1,12 +1,21 @@
-function Home() {
+function Home(){
+
+  const ctx = React.useContext(UserContext);
+
+  const name = ctx.users[0].name;
+  const email = ctx.users[0].email;
+
+  const welcomeMessage = "Welcome to the bank " + name + "!";
+  const textMessage = "You can move around using the navigation bar " + email + "."
+
   return (
     <Card
-      bgcolor="primary"
-      txtcolor="light"
+      txtcolor="black"
+      bgcolor="red"
       header="BadBank Landing Module"
-      title="Welcome to Bad Bank!"
-      text="Interact with the menu to begin."
-      body={<img src="bank.png" className="img-fluid" alt="Responsive image" />}
-    />
-  );
+      title={welcomeMessage}
+      text={textMessage}
+      body={(<img src="bank.png" className="img-fluid" alt="Responsive image"/>)}
+    />    
+  );  
 }
